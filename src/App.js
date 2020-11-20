2726f3a41571b2f978601c1a9ff492d3009e813d
Ignacio <ignaciotagle26@gmail.com>
2020-11-20 00:44:22 -0300

    Proyecto para deployear

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Fragment, useState, useEffect } from 'react';
+import Cita from './components/Cita';
+import Formulario from './components/Formulario';
+
+function App() {
+    //Citas en localStorage
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
+    if (!citasIniciales) {
+        citasIniciales = [];
+    }
+
+    // Array de citas
+    const [citas, guardarCitas] = useState(citasIniciales);
+
+    // Use effect para realizar ciertas operaciones cuando el state cambia
+    useEffect(() => {
+        localStorage.setItem('citas', JSON.stringify(citas));
+    }, [citas]);
+
+    const crearCita = cita => {
+        guardarCitas([...citas, cita]);
+    };
+
+    const eliminarCita = id => {
+        const nuevasCitas = citas.filter(cita => cita.id !== id);
+
+        guardarCitas(nuevasCitas);
+    };
+
+    const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+
+    return (
+        <Fragment>
+            <h1>Administrador de Pacientes</h1>
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='one-half column'>
+                        <Formulario crearCita={crearCita} />
+                    </div>
+                    <div className='one-half column'>
+                        <h2>{titulo}</h2>
+                        {citas.map(cita => (
+                            <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+export default App;
